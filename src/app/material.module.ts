37284a6ae14b01de9666d824ece1f422ce0bc0a8
Ishanz23 <sayantandey23@gmail.com>
2018-02-08 12:00:33 +0530

    Materialized UI and connected the components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,34 @@
 import {NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
-    imports: [ MatButtonModule, MatInputModule, MatIconModule ],
-    exports: [ MatButtonModule, MatInputModule, MatIconModule ]
+    imports: [
+        MatButtonModule,
+        MatInputModule,
+        MatIconModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatListModule,
+        MatCardModule,
+        MatDividerModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatInputModule,
+        MatIconModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatListModule,
+        MatCardModule
+    ]
 })
 
 export class MaterialModule  { }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,17 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [new Recipe('Dal Fry',
   'The tasty companion for your Chapatis!',
   'https://upload.wikimedia.org/wikipedia/commons/3/39/Daal_after_Tadka_Pulse_Soup_India.jpg'),
   new Recipe('Palak Paneer',
-  'The Magic of spincah with Paneer!',
-  'https://upload.wikimedia.org/wikipedia/commons/3/39/Daal_after_Tadka_Pulse_Soup_India.jpg')];
+  'The Magic of spinach with Paneer!',
+  'https://upload.wikimedia.org/wikipedia/commons/d/da/Yummy_Palak_Paneer.jpg')];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
